b67f4ff8d77bc5f8478dc19466b209dfd7323200
b-smith94 <byrondsmith9@gmail.com>
2024-08-27 13:07:30 -0400

    Added another module to edit strings, then imported it as an alias and applied its functions to a text.

---
+++ mastering_python_modules_and_aliases.py
@@ -0,0 +1,6 @@
+import text_utils as tu
+
+txt = "hello, world!"
+
+print(tu.reverse_string(txt))
+print(tu.capitalize_string(txt))
---
+++ text_utils.py
@@ -0,0 +1,7 @@
+def reverse_string(s):
+    text = s[::-1]
+    return(text)
+
+def capitalize_string(s):
+    capital_text = s.capitalize()
+    return(capital_text)
